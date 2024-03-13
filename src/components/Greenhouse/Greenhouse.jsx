2b3191d1f63237d31f79247ee3e6316ed9f9595c
garciaa38 <garciaa38@gmail.com>
2024-03-13 18:34:04 -0400

    phase 1 and 2 complete

--- src/components/Greenhouse/Greenhouse.jsx
+++ src/components/Greenhouse/Greenhouse.jsx
@@ -1,18 +1,24 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
+import { useTheme } from '../../context/ThemeContext';
 import './Greenhouse.css';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
+
 function Greenhouse() {
+  
+  const {themeName} = useTheme();
+
+  console.log(themeName)
 
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
+      <div>
+      {themeName === "day" ? (<img className='greenhouse-img' src={dayImage} alt='greenhouse'/>) : 
+      (<img className='greenhouse-img' src={nightImage} alt='greenhouse'/>)}
+      </div>
       <LightSwitch />
       <ClimateStats />
     </section>
